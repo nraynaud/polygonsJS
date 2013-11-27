43452fdf5bfbb24c5495b334f06106e060fbad4b
Nicolas Raynaud <github@nraynaud.com>
2013-11-27 15:52:40 +0100

    completed Bentley Ottmann with mock underlying structures.

--- polygons.js
+++ polygons.js
@@ -2,17 +2,21 @@
 
 //http://www.kevlindev.com/gui/math/intersection/Intersection.js
 function intersectionSegments(seg1, seg2) {
+  if (comparePoint(seg1[0], seg2[0]) > 0) {
+    var tmp = seg1;
+    seg1 = seg2;
+    seg2 = tmp;
+  }
   var a1 = seg1[0], a2 = seg1[1], b1 = seg2[0], b2 = seg2[1];
   var ua_t = (b2.x - b1.x) * (a1.y - b1.y) - (b2.y - b1.y) * (a1.x - b1.x);
   var ub_t = (a2.x - a1.x) * (a1.y - b1.y) - (a2.y - a1.y) * (a1.x - b1.x);
-  var u_b = (b2.y - b1.y) * (a2.x - a1.x) - (b2.x - b1.x) * (a2.y - a1.y);
+  var divisor = (b2.y - b1.y) * (a2.x - a1.x) - (b2.x - b1.x) * (a2.y - a1.y);
 
-  if (u_b != 0) {
-    var ua = ua_t / u_b;
-    var ub = ub_t / u_b;
-    if (0 <= ua && ua <= 1 && 0 <= ub && ub <= 1) {
+  if (divisor != 0) {
+    var ua = ua_t / divisor;
+    var ub = ub_t / divisor;
+    if (0 <= ua && ua <= 1 && 0 <= ub && ub <= 1)
       return {x: a1.x + ua * (a2.x - a1.x), y: a1.y + ua * (a2.y - a1.y)};
-    }
   }
   return null;
 }
@@ -20,43 +24,104 @@ function intersectionSegments(seg1, seg2) {
 function createIntersections(segments) {
   var intersections = [];
   for (var i = 0; i < segments.length; i++)
+    if (comparePoint(segments[i][0], segments[i][1]) > 0)
+      segments[i].reverse();
+  for (i = 0; i < segments.length; i++) {
     for (var j = i + 1; j < segments.length; j++) {
       var intersection = intersectionSegments(segments[i], segments[j]);
       if (intersection)
-        intersections.push(intersection);
+        intersections.push([intersection, [segments[i], segments[j]]]);
     }
+  }
   return intersections;
 }
 
-function createIntersectionsBentley(segments) {
-  var initialEvents = [];
-  for (var i = 0; i < segments.length; i++) {
-    var segment = segments[i];
-    initialEvents.push({point: segment[0]});
-    initialEvents.push({point: segment[1]});
-  }
-  var queue = createEventQueue(initialEvents);
-  return  queue;
+function comparePoint(p1, p2) {
+  if (p1.x < p2.x)
+    return -1;
+  if (p1.x == p2.x)
+    return p1.y - p2.y;
+  return 1;
 }
 
-function createEventQueue(initialEvents) {
+function eventsForSegment(segment) {
+  var s = [segment[0], segment[1]];
+  if (comparePoint(segment[0], segment[1]) > 0)
+    s.reverse();
+  return [
+    {type: 'left', point: s[0], segment: s},
+    {type: 'right', point: s[1], segment: s}
+  ];
+}
+
+function initialPopulationOfQueue(segments) {
+  var initialEvents = [];
+  for (var i = 0; i < segments.length; i++) {
+    var events = eventsForSegment(segments[i]);
+    initialEvents.push(events[0]);
+    initialEvents.push(events[1]);
+  }
+  return createMockEventQueue(initialEvents);
+}
+
+function bentleyOttmann(segments) {
+  var queue = initialPopulationOfQueue(segments);
+  var scanBeam = createMockScanBeam();
+  while (!queue.isEmpty()) {
+    var event = queue.fetchFirst();
+    switch (event.type) {
+      case 'left':
+        scanBeam.leftPoint(event.point, event.segment, queue);
+        break;
+      case 'right':
+        scanBeam.rightPoint(event.point, event.segment, queue);
+        break;
+      case 'intersection':
+        scanBeam.intersectionPoint(event.point, event.segments, queue);
+        break;
+    }
+  }
+  return scanBeam.getResult();
+}
+
+function createMockEventQueue(initialEvents) {
   var queue = [];
 
+  function pointEquals(p1, p2) {
+    return p1.x == p2.x && p1.y == p2.y;
+  }
+
+  function segmentEqual(s1, s2) {
+    return s1 == s2
+        || pointEquals(s1[0], s2[0]) && pointEquals(s1[1], s2[1])
+        || pointEquals(s1[1], s2[0]) && pointEquals(s1[0], s2[1]);
+  }
+
+  function eventExists(event) {
+    for (var i = 0; i < queue.length; i++)
+      if (queue[i].type == 'intersection'
+          && pointEquals(queue[i].point, event.point)
+          && (segmentEqual(event.segments[0], queue[i].segments[0]) && segmentEqual(event.segments[1], queue[i].segments[1])
+          || segmentEqual(event.segments[1], queue[i].segments[0]) && segmentEqual(event.segments[0], queue[i].segments[1])))
+        return true;
+    return false
+  }
+
+  function compareEvents(e1, e2) {
+    return comparePoint(e1.point, e2.point);
+  }
+
   function sort() {
-    queue.sort(function (e1, e2) {
-      if (e1.point.x < e2.point.x)
-        return -1;
-      if (e1.point.x == e2.point.x)
-        return e1.point.y - e2.point.y;
-      return 1;
-    });
+    queue.sort(compareEvents);
   }
 
   Array.prototype.push.apply(queue, initialEvents);
   sort();
   return {
-    push: function (element) {
-      queue.push(element);
+    pushIntersectionEvent: function (event) {
+      if (eventExists(event))
+        return;
+      queue.push(event);
       sort();
     },
     fetchFirst: function () {
@@ -64,7 +129,88 @@ function createEventQueue(initialEvents) {
     },
     isEmpty: function () {
       return queue.length == 0;
+    },
+    dumpQueue: function () {
+      var res = [];
+      for (var i = 0; i < queue.length; i++)
+        res.push(queue[i]);
+      return res;
     }
   }
 }
 
+function createMockScanBeam() {
+  var beam = [];
+  var result = [];
+
+  function findIndex(point) {
+    function segmentHeight(segment, x) {
+      if (segment[0].x > segment[1].x)
+        throw 'backwards segment';
+      var t = (x - segment[0].x) / (segment[1].x - segment[0].x);
+      return segment[0].y + t * (segment[1].y - segment[0].y);
+    }
+
+    for (var i = 0; i < beam.length; i++) {
+      var segmentH = segmentHeight(beam[i], point.x);
+      if (segmentH > point.y)
+        return i;
+    }
+    return beam.length;
+  }
+
+  function findSegmentIndex(segment) {
+    for (var i = 0; i < beam.length; i++)
+      if (beam[i] == segment)
+        return i;
+    throw 'oops segment not in beam???';
+  }
+
+  function swap(i1, i2) {
+    var tmp = beam[i1];
+    beam[i1] = beam[i2];
+    beam[i2] = tmp;
+  }
+
+  function pushIfIntersectsOnRight(currentPoint, previousSegment, nextSegment, eventQueue) {
+    var intersection = intersectionSegments(nextSegment, previousSegment);
+    if (intersection != null && comparePoint(currentPoint, intersection) < 0)
+      eventQueue.pushIntersectionEvent({type: 'intersection', point: intersection, segments: [previousSegment, nextSegment]});
+  }
+
+  return {
+    leftPoint: function (point, segment, eventQueue) {
+      var insertionIndex = findIndex(point);
+      if (insertionIndex > 0)
+        pushIfIntersectsOnRight(point, beam[insertionIndex - 1], segment, eventQueue);
+      if (insertionIndex < beam.length)
+        pushIfIntersectsOnRight(point, segment, beam[insertionIndex], eventQueue);
+      beam.splice(insertionIndex, 0, segment);
+    },
+    rightPoint: function (point, segment, eventQueue) {
+      var segmentIndex = findSegmentIndex(segment);
+      if (segmentIndex > 0 && segmentIndex < beam.length - 1)
+        pushIfIntersectsOnRight(point, beam[segmentIndex - 1], beam[segmentIndex + 1], eventQueue);
+      beam.splice(segmentIndex, 1);
+    },
+    intersectionPoint: function (point, segments, eventQueue) {
+      result.push([point, segments]);
+      var before = findSegmentIndex(segments[0]);
+      var after = findSegmentIndex(segments[1]);
+      swap(before, after);
+      if (before > 0)
+        pushIfIntersectsOnRight(point, beam[before - 1], segments[1], eventQueue);
+      if (after < beam.length - 1)
+        pushIfIntersectsOnRight(point, segments[0], beam[after + 1], eventQueue);
+    },
+    getResult: function () {
+      return result;
+    },
+    dumpBeam: function () {
+      var res = [];
+      for (var i = 0; i < beam.length; i++)
+        res.push(beam[i]);
+      return res;
+    }
+  };
+}
--- test.js
+++ test.js
@@ -28,16 +28,9 @@ function svgAssertTable(inputPath, outputPath, expectedPath) {
       + row + '</tr></table></div>';
 }
 
-test('test1', function () {
-  var inputPath = 'M10,10L20,10L20,20L10,20L10,10Z';
-  var outputPath = 'M10,10L20,10L20,20L10,20L10,10Z';
-  var expectedPath = 'M10,10L20,10L20,20L10,20L10,10Z';
-  equal(outputPath, expectedPath);
-  QUnit.config.current.assertions.push({
-    result: true,
-    message: svgAssertTable(inputPath, outputPath, expectedPath)
-  });
-});
+function randomPoint() {
+  return p(Math.random() * 200, Math.random() * 150);
+}
 
 function p(x, y) {
   return {x: x, y: y};
@@ -80,6 +73,13 @@ function displaySegmentsAndPoint(segments, expectedPoint, resultPoints) {
   });
 }
 
+function cleanupIntersectionsForDisplay(intersections) {
+  var res = [];
+  for (var i = 0; i < intersections.length; i++)
+    res.push(intersections[i][0]);
+  return res;
+}
+
 test('2 segments intersections', function () {
   var s1 = [p(0, 0), p(20, 20)];
   var s2 = [p(20, 0), p(0, 20)];
@@ -89,6 +89,35 @@ test('2 segments intersections', function () {
   displaySegmentsAndPoint([s1, s2], [expected], [res]);
 });
 
+test('horizontal-vertical intersection', function () {
+  var s1 = [p(0, 30), p(60, 30)];
+  var s2 = [p(30, 0), p(30, 60)];
+  var res = intersectionSegments(s1, s2);
+  var expected = {x: 30, y: 30};
+  deepEqual(res, expected);
+  displaySegmentsAndPoint([s1, s2], [expected], [res]);
+});
+
+test('2 almost horizontal segments intersections', function () {
+  var epsilon = Math.pow(2, -50);
+  var big = Math.pow(2, 52);
+  var s1 = [p(1 - big, 1 - epsilon), p(1 + big, 1 + epsilon)];
+  var s2 = [p(1 - big, 1 + epsilon), p(1 + big, 1 - epsilon)];
+  var res = intersectionSegments(s1, s2);
+  var expected = {x: 1, y: 1};
+  deepEqual(res, expected);
+});
+
+test('2 almost vertical segments intersections', function () {
+  var epsilon = Math.pow(2, -50);
+  var big = Math.pow(2, 52);
+  var s1 = [p(1 - epsilon, 1 - big), p(1 + epsilon, 1 + big)];
+  var s2 = [p(1 + epsilon, 1 - big), p(1 - epsilon, 1 + big)];
+  var res = intersectionSegments(s1, s2);
+  var expected = {x: 1, y: 1};
+  deepEqual(res, expected);
+});
+
 test('3 segments array intersections', function () {
   var s1 = [p(0, 0), p(20, 20)];
   var s2 = [p(20, 0), p(0, 20)];
@@ -96,22 +125,18 @@ test('3 segments array intersections', function () {
   var segments = [s1, s2, s3];
   var res = createIntersections(segments);
   var expected = [
-    {x: 10, y: 10},
-    {x: 5, y: 15}
+    [
+      {x: 10, y: 10},
+      [s1, s2]
+    ],
+    [
+      {x: 5, y: 15},
+      [s2, s3]
+    ]
   ];
   deepEqual(res, expected);
-  displaySegmentsAndPoint(segments, res, expected);
-});
-
-function randomPoint() {
-  return p(Math.random() * 200, Math.random() * 150);
-}
-test('random segments O(n^2) intersections', function () {
-  var segments = [];
-  for (var i = 0; i < 50; i++)
-    segments.push([randomPoint(), randomPoint()]);
-  var res = createIntersections(segments);
-  var expected = [];
+  expected = cleanupIntersectionsForDisplay(expected);
+  res = cleanupIntersectionsForDisplay(res);
   displaySegmentsAndPoint(segments, res, expected);
 });
 
@@ -121,7 +146,7 @@ test('initialization of event queue', function () {
   var s2 = [expected[4], expected[2]];
   var s3 = [expected[1], expected[3]];
   var segments = [s1, s2, s3];
-  var queue = createIntersectionsBentley(segments);
+  var queue = initialPopulationOfQueue(segments);
   var res = [];
   ok(!queue.isEmpty(), 'queue is not empty');
   while (!queue.isEmpty())
@@ -135,9 +160,9 @@ test('adding event to queue', function () {
   var expected = [p(0, 0), p(0, 20), p(10, 20), p(20, 20)];
   var s1 = [expected[0], expected[3]];
   var segments = [s1];
-  var queue = createIntersectionsBentley(segments);
-  queue.push({point: expected[2]});
-  queue.push({point: expected[1]});
+  var queue = initialPopulationOfQueue(segments);
+  queue.pushIntersectionEvent({point: expected[2]});
+  queue.pushIntersectionEvent({point: expected[1]});
   var res = [];
   ok(!queue.isEmpty(), 'queue is not empty');
   while (!queue.isEmpty())
@@ -145,4 +170,120 @@ test('adding event to queue', function () {
   deepEqual(res, expected, 'events in correct order');
   ok(queue.isEmpty(), 'queue is empty after dumping the events');
   displaySegmentsAndPoint(segments, res, expected);
+});
+
+test('adding left events to beam', function () {
+  var s1 = [p(0, 0), p(20, 20)];
+  var s2 = [p(20, 0), p(0, 20)];
+  var s3 = [p(0, 10), p(5, 10)];
+  var segments = [s1, s2, s3];
+  var res = bentleyOttmann(segments);
+  var expected = [p(10, 10)];
+  res = cleanupIntersectionsForDisplay(res);
+  deepEqual(res, expected);
+  displaySegmentsAndPoint(segments, expected, res);
+});
+
+test('beam 2 segments', function () {
+  var s1 = [p(0, 0), p(20, 20)];
+  var s2 = [p(20, 0), p(0, 20)];
+  var queue = createMockEventQueue([]);
+  var beam = createMockScanBeam();
+  beam.leftPoint(s1[0], s1, queue);
+  beam.leftPoint(s2[1], s2, queue);
+  deepEqual(beam.dumpBeam(), [s1, s2]);
+  deepEqual(queue.dumpQueue(), [
+    {type: 'intersection', point: p(10, 10), segments: [s1, s2]}
+  ]);
+  beam.intersectionPoint(p(10, 10), [s1, s2], queue);
+  deepEqual(beam.dumpBeam(), [s2, s1]);
+  deepEqual(beam.getResult(), [
+    [
+      p(10, 10),
+      [s1, s2]
+    ]
+  ]);
+  beam.rightPoint(s2[0], s2, queue);
+  deepEqual(beam.dumpBeam(), [s1]);
+  beam.rightPoint(s1[1], s1, queue);
+  deepEqual(beam.dumpBeam(), []);
+  deepEqual(beam.getResult(), [
+    [
+      {x: 10, y: 10},
+      [s1, s2]
+    ]
+  ]);
+});
+
+test('beam 4 segments; intersection masked by intersection', function () {
+  var s1 = [p(0, 0), p(20, 20)];
+  var s2 = [p(20, 0), p(0, 20)];
+  var s3 = [p(7.5, 5), p(20, 5)];
+  var s4 = [p(7.5, 15), p(20, 15)];
+  var segments = [s1, s2, s3, s4];
+  var res = bentleyOttmann(segments);
+  var expected = [p(10, 10), p(15, 5), p(15, 15)];
+  res = cleanupIntersectionsForDisplay(res);
+  deepEqual(res, expected);
+  displaySegmentsAndPoint(segments, expected, res);
+});
+
+test('beam 3 segments; intersection masked by right segment', function () {
+  var s1 = [p(5, 0), p(20, 20)];
+  var s2 = [p(20, 0), p(5, 20)];
+  var s3 = [p(0, 10), p(10, 10)];
+  var segments = [s1, s2, s3];
+  var res = bentleyOttmann(segments);
+  var expected = [p(12.5, 10)];
+  res = cleanupIntersectionsForDisplay(res);
+  deepEqual(res, expected);
+  displaySegmentsAndPoint(segments, expected, res);
+});
+
+test('beam 3 segments; intersecting beam comes late', function () {
+  var s1 = [p(0, 0), p(200, 110)];
+  var s2 = [p(0, 150), p(150, 90)];
+  var s3 = [p(50, 100), p(200, 100)];
+  var segments = [s1, s2, s3];
+  var res = bentleyOttmann(segments);
+  var expected = [p(125, 100), p(181.8181818181818, 100)];
+  res = cleanupIntersectionsForDisplay(res);
+  deepEqual(res, expected);
+  displaySegmentsAndPoint(segments, expected, res);
+});
+
+test('eventQueue prevents double insertion', function () {
+  var s1 = [p(0, 0), p(20, 20)];
+  var s2 = [p(20, 0), p(0, 20)];
+  var queue = createMockEventQueue([]);
+  var event = {type: 'intersection', point: p(10, 10), segments: [s1, s2]};
+  queue.pushIntersectionEvent(event);
+  deepEqual(queue.dumpQueue(), [event]);
+  queue.pushIntersectionEvent({type: 'intersection', point: p(10, 10), segments: [s1, s2]});
+  deepEqual(queue.dumpQueue(), [event]);
+});
+
+test('small random segment batch O(n^2) intersections against mock Bentley Ottmann', function () {
+  var segments = [];
+  for (var i = 0; i < 30; i++)
+    segments.push([randomPoint(), randomPoint()]);
+  var expected = cleanupIntersectionsForDisplay(createIntersections(segments));
+  var res = cleanupIntersectionsForDisplay(bentleyOttmann(segments));
+  expected.sort(comparePoint);
+  res.sort(comparePoint);
+  displaySegmentsAndPoint(segments, expected, res);
+  deepEqual(res.length, expected.length, 'same number of points as expected');
+  deepEqual(res, expected, 'intersections are equal');
+});
+
+test('big random segment batch O(n^2) intersections against mock Bentley Ottmann', function () {
+  var segments = [];
+  for (var i = 0; i < 300; i++)
+    segments.push([randomPoint(), randomPoint()]);
+  var expected = cleanupIntersectionsForDisplay(createIntersections(segments));
+  var res = cleanupIntersectionsForDisplay(bentleyOttmann(segments));
+  expected.sort(comparePoint);
+  res.sort(comparePoint);
+  deepEqual(res.length, expected.length, 'same number of points as expected');
+  deepEqual(res, expected, 'intersections are equal');
 });
